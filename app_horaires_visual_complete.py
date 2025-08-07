d064a142912b60973d3da5ff5aac554b85ef6107
Marindodouce0 <julienmartineau1@hotmail.com>
2025-08-07 10:57:26 -0400

    Update app_horaires_visual_complete.py

--- app_horaires_visual_complete.py
+++ app_horaires_visual_complete.py
@@ -910,7 +910,7 @@ with tab4:
             st.subheader("📊 Export Excel")
             if st.button("Générer le fichier Excel", use_container_width=True):
                 output = BytesIO()
-                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                with pd.ExcelWriter(output, engine='openpyxl') as writer:
                     # Feuille 1: Horaires complets
                     all_data = []
                     for cheval in sorted(st.session_state.schedule.keys()):
@@ -951,3 +951,4 @@ st.markdown("""
 </div>
 
 """, unsafe_allow_html=True)
+
